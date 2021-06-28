5746cf531b9f3b417cecce86adc3e3b69e624844
kinyeramos2021 <kinyeramo@gmail.com>
2021-06-29 02:19:17 +0300

    webpack setup

---
+++ client/client.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import ReactDOM  from 'react-dom';
+
+const App = ()=>(
+    <div>
+        This is a React component!
+    </div>
+)
+
+ReactDOM.render(<App/>, document.querySelector("#container"));
