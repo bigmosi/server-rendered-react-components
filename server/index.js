560dbdf1ae886ca9b7c0401b828e493ef02aa747
kinyeramos2021 <kinyeramo@gmail.com>
2021-06-29 23:23:35 +0300

    rendered to string react App

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import react from 'react';
 import express from "express";
 import { readFileSync } from 'fs';
+import { renderToString } from 'react-dom/server';
 
 import { App } from '../client/App';
 
@@ -9,6 +10,7 @@ app.use(express.static("dist"));
 
 app.get('/', async(_req,res) => {
    const index = readFileSync(`public/index.html`, `utf8`);
+   const rendered = renderToString(<App/>);
    res.send(index);
 
    // res.send(
