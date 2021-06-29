f18890eda4c0462a0d49ade01b9e64ffe0f60f02
kinyeramos2021 <kinyeramo@gmail.com>
2021-06-30 02:29:18 +0300

    adding button to app

--- client/client.jsx
+++ client/client.jsx
@@ -2,6 +2,15 @@ import react from 'react';
 import ReactDOM  from 'react-dom';
 import { App } from './App';
 
+let state = undefined;
+
+fetch("http://localhost:7777/data")
+     .then(data => data.json())
+     .then(json =>{
+         state = json;
+         console.log("Got the state", state);
+         render();
+     })
 
 //ReactDOM.render(<App/>, document.querySelector("#container"));
 
@@ -10,4 +19,4 @@ function render() {
     ReactDOM.hydrate(<App/>, querySelector("#container"));
 }
 
-render();
+//render();
