3319a3d851674ca0fea12d692eab185e2fc2aeb1
kinyeramos2021 <kinyeramo@gmail.com>
2021-06-30 01:01:49 +0300

    React component organising state in the way that make sense

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,29 @@
 import react from 'react';
 
 
-export const App = ()=>(
+export const App = ({questions, answers})=>(
     <div>
-    This is a React component!
+    <h1>
+        Q&A Tool
+    </h1>
+
+    {questions.map(({questionId, content})=>(
+    <div key={questionId}>
+        <h3>
+            {content}
+        </h3>
+        <div>
+            {answers.filter(answer => answer.questionId === questionId).map(
+                ({content, upvotes, answerId})=>(
+                    <div key={answerId}>
+                        <span>
+                            {content} - {upvotes}
+                        </span>
+                    </div>
+                )
+            )}
+        </div>
+    </div>
+    ))}
     </div>
 );
