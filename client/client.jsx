7f5e9c7334f9c6ffd5b52ec4c3e0280ad0f028d7
kinyeramos2021 <kinyeramo@gmail.com>
2021-06-30 01:58:03 +0300

    adding interactivity to server rendered components through rehydration

--- client/client.jsx
+++ client/client.jsx
@@ -1,6 +1,13 @@
 import react from 'react';
 import ReactDOM  from 'react-dom';
-import {App} from './App';
+import { App } from './App';
 
 
-ReactDOM.render(<App/>, document.querySelector("#container"));
+//ReactDOM.render(<App/>, document.querySelector("#container"));
+
+function render() {
+
+    ReactDOM.hydrate(<App/>, querySelector("#container"));
+}
+
+render();
